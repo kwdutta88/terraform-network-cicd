d03131ed73a4775d5111f22ba1a300e299b2d87b
KRISHNAKALI DUTTA <165360235+kwdutta88@users.noreply.github.com>
2025-03-15 20:13:57 +0530

    Update outputs.tf

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,11 +1,35 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_id" {
-  value = aws_subnet.public.id
+  description = "Public Subnet ID"
+  value       = aws_subnet.public.id
 }
 
 output "private_subnet_id" {
-  value = aws_subnet.private.id
+  description = "Private Subnet ID"
+  value       = aws_subnet.private.id
 }
+
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = aws_internet_gateway.gw.id
+}
+
+output "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  value       = aws_nat_gateway.nat.id
+}
+
+output "public_route_table_id" {
+  description = "Public Route Table ID"
+  value       = aws_route_table.public_rt.id
+}
+
+output "private_route_table_id" {
+  description = "Private Route Table ID"
+  value       = aws_route_table.private_rt.id
+}
+
