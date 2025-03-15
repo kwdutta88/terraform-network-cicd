aba7061ce0442dda99efa2f5738c78d739d21050
KRISHNAKALI DUTTA <165360235+kwdutta88@users.noreply.github.com>
2025-03-15 20:05:07 +0530

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,4 +1,30 @@
-variable "vpc_cidr" {}
-variable "public_subnet_cidr" {}
-variable "private_subnet_cidr" {}
-variable "availability_zone" {}
+variable "vpc_name" {
+  description = "Name of the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "availability_zone" {
+  description = "AWS availability zone"
+  type        = string
+}
+
+variable "nat_gateway_enabled" {
+  description = "Enable NAT Gateway for private subnet"
+  type        = bool
+  default     = true
+}
