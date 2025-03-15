024d2e3db8f88a2c22083cf1f8035db020f76a4e
KRISHNAKALI DUTTA <165360235+kwdutta88@users.noreply.github.com>
2025-03-15 20:12:49 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,23 @@ output "private_subnet_id" {
   description = "Private Subnet ID"
   value       = module.vpc.private_subnet_id
 }
+
+output "internet_gateway_id" {
+  description = "Internet Gateway ID"
+  value       = module.vpc.internet_gateway_id
+}
+
+output "nat_gateway_id" {
+  description = "NAT Gateway ID"
+  value       = module.vpc.nat_gateway_id
+}
+
+output "public_route_table_id" {
+  description = "Public Route Table ID"
+  value       = module.vpc.public_route_table_id
+}
+
+output "private_route_table_id" {
+  description = "Private Route Table ID"
+  value       = module.vpc.private_route_table_id
+}
