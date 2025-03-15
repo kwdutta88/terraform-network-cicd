1584b642b3b03bdafe36fa29ae63e357acc64e17
KRISHNAKALI DUTTA <165360235+kwdutta88@users.noreply.github.com>
2025-03-15 20:06:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "vpc" {
   source              = "./modules/vpc"
-  vpc_cidr           = var.vpc_cidr
-  public_subnet_cidr = var.public_subnet_cidr
+  vpc_name            = var.vpc_name
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
   availability_zone   = var.availability_zone
+  nat_gateway_enabled = var.nat_gateway_enabled
 }
