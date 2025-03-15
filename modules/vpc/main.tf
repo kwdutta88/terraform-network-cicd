971a337719da5713d03196e2e256f28f2c2ce038
KRISHNAKALI DUTTA <165360235+kwdutta88@users.noreply.github.com>
2025-03-15 19:57:18 +0530

    Update main.tf
    added the missing route tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -21,11 +21,19 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_eip" "nat" {}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+}
+
+# Public Route Table (Routes all traffic to the Internet Gateway)
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 }
 
-resource "aws_route" "default_route" {
+resource "aws_route" "public_internet_access" {
   route_table_id         = aws_route_table.public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.gw.id
@@ -34,4 +42,20 @@ resource "aws_route" "default_route" {
 resource "aws_route_table_association" "public_assoc" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public_rt.id
+}
+
+# Private Route Table (Routes all outbound traffic to the NAT Gateway)
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route" "private_nat_gateway_route" {
+  route_table_id         = aws_route_table.private_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
 }
